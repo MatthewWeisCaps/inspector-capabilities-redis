5aeae3a64d4d94a1b828f031f9fc936ec8363216
Matthew Weis <matthewweiscaps@gmail.com>
2020-02-14 14:03:19 -0600

    initial structure

---
+++ src/main/java/org/sireum/hamr/inspector/capabilities/redis/Commands.java
@@ -0,0 +1,85 @@
+package org.sireum.hamr.inspector.capabilities.redis;
+
+import art.Art;
+import art.DataContent;
+import io.lettuce.core.SetArgs;
+import io.lettuce.core.api.sync.RedisCommands;
+import org.sireum.hamr.inspector.capabilities.SlangInspector$;
+
+public final class Commands {
+
+  public static final String NUM_SESSIONS_KEY = "numSessions";
+
+  private static RedisCommands<String, String> cmds = null;
+  private static String startKey = null;
+  private static String stopKey = null;
+  private static String streamKey = null;
+
+  public static String incomingChannelKey = null;
+  private static final String outgoingChannelKey = "inspector-channel";
+
+  public static synchronized void init(RedisCommands<String, String> cmds) {
+    if (startKey != null || stopKey != null || streamKey != null || incomingChannelKey != null) {
+      throw new IllegalStateException("Commands.init() in inspector-capabilities-redis can only be initialized once!");
+    }
+
+    Commands.cmds = cmds;
+
+    // if the redis server does NOT contain NUM_SESSIONS_KEY, then initialize it to 0
+    cmds.setnx(NUM_SESSIONS_KEY, "0");
+
+    // atomically increment session counter, returning this session's unique key
+    final String streamName = Long.toString(cmds.incr(NUM_SESSIONS_KEY));
+    startKey = streamName + "-start";
+    stopKey = streamName + "-stop";
+    streamKey = streamName + "-stream";
+    incomingChannelKey = streamName + "-channel";
+
+    // add shutdown hook to set shutdown time if the app did not stop naturally
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      stop(Art.time().toString(), "JVM Shutdown Hook");
+    }));
+  }
+
+  public static void start(String timestamp) {
+    if (startKey == null || stopKey == null || streamKey == null || incomingChannelKey == null) {
+      throw new IllegalStateException("Commands.init() must be called before Commands.start() in inspector-capabilities-redis.");
+    }
+
+    // set the value of startKey, but only if it does not already exist
+    final String reply = cmds.set(startKey, timestamp, SetArgs.Builder.nx());
+
+    // throw error if something went wrong
+    if (!reply.equals("OK")) {
+      throw new IllegalStateException("Commands.start() in inspector-capabilities-redis was unable to set startKey");
+    }
+
+    cmds.publish(outgoingChannelKey, startKey);
+  }
+
+  public static void stop(String timestamp, String cause) {
+    if (startKey == null || stopKey == null || streamKey == null || incomingChannelKey == null) {
+      throw new IllegalStateException("Commands.init() must be called before Commands.stop() in inspector-capabilities-redis.");
+    }
+
+    // set the value of stopKey, but only if it does not already exist
+    final String reply = cmds.set(stopKey, timestamp, SetArgs.Builder.nx());
+
+    // Tell the Inspector that this project is shutting down, but only if this is the first time calling the method.
+    // This is because shutdown hook and natural shutdown may cause two calls to this method.
+    if (reply.equals("OK")) {
+      cmds.publish(outgoingChannelKey, stopKey);
+      cmds.xadd(streamKey, "timestamp", timestamp, "stop", cause);
+    }
+  }
+
+  public static void xadd(String timestamp, long src, long dst, DataContent data) {
+    final String json = SlangInspector$.MODULE$.serializer().apply(data);
+    cmds.xadd(streamKey, "timestamp", timestamp, "src", Long.toString(src), "dst", Long.toString(dst), "data", json);
+  }
+
+  private Commands() {
+    throw new IllegalStateException("Commands cannot be instantiated.");
+  }
+
+}
---
+++ src/main/scala/org/sireum/hamr/inspector/capabilities/redis/DirectProjectListener.scala
@@ -0,0 +1,68 @@
+package org.sireum.hamr.inspector.capabilities.redis
+
+import java.util.concurrent.Executors
+
+import art.Art.PortId
+import art.{Art, ArtDebug, DataContent}
+import io.lettuce.core.RedisClient
+import io.lettuce.core.api.StatefulRedisConnection
+import io.lettuce.core.api.sync.RedisCommands
+import io.lettuce.core.pubsub.{RedisPubSubAdapter, StatefulRedisPubSubConnection}
+import org.sireum.Z
+import org.sireum.hamr.inspector.capabilities.{ProjectListener, SlangInspector}
+
+final class DirectProjectListener extends ProjectListener {
+
+  private val redisClient = RedisClient.create("redis://localhost:6379/0")
+  private val connection: StatefulRedisConnection[String, String] = redisClient.connect()
+  private val pubSubConnection: StatefulRedisPubSubConnection[String, String] = redisClient.connectPubSub()
+  private val commands: RedisCommands[String, String] = connection.sync()
+
+  Commands.init(commands)
+
+  private val executorThreadGroup = new ThreadGroup("Inspector")
+  private val executorThreadName = "inspector-capabilities-redis-EXECUTOR"
+
+  private val executor =
+    Executors.newSingleThreadExecutor((r: Runnable) => new Thread(executorThreadGroup, r, executorThreadName))
+
+  override def start(time: Art.Time): Unit = {
+    Commands.start(time.toString)
+
+    pubSubConnection.addListener(new RedisPubSubAdapter[String, String] {
+      override def message(channel: String, message: String): Unit = {
+
+        println(s"Inspector-Capabilities-Redis INFO: handling remote injection: $message")
+
+        val split = message.split(',')
+
+        if (split.length < 3) {
+          println("Inspector-Capabilities-Redis WARNING: DirectProjectListener received malformed message {}", message)
+          return
+        }
+
+        val bridgeId: Art.BridgeId = Z.apply(split(0)).getOrElse(Z(-1))
+        val portId: Art.PortId = Z.apply(split(1)).getOrElse(Z(-1))
+
+        val dataContentStartIndex = split(0).length + split(1).length + 2 // plus 2 for extra two commas in init String
+        val dataContentString = message.substring(dataContentStartIndex)
+        val dataContent = SlangInspector.deserializer(dataContentString)
+
+        ArtDebug.injectPort(bridgeId, portId, dataContent)
+      }
+    })
+
+    pubSubConnection.sync().subscribe(Commands.incomingChannelKey)
+  }
+
+  override def stop(time: Art.Time): Unit = {
+    Commands.stop(time.toString, "Natural Termination")
+  }
+
+  override def output(src: PortId, dst: PortId, data: DataContent, time: Art.Time): Unit = {
+    executor.execute(() => {
+      Commands.xadd(time.toString, src.toLong, dst.toLong, data)
+    })
+  }
+
+}
